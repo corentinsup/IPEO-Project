6e24ad6d36f79041cee698d736b3c24d1038e062
Jérémy Hugentobler <jeremy.hugentobler@epfl.ch>
2025-12-23 20:43:53 +0100

    Full training script

---
+++ models/utils/metrics.py
@@ -0,0 +1,53 @@
+# Utility file with function for the metrics
+import segmentation_models_pytorch as smp
+from torchmetrics import JaccardIndex
+import torch.nn as nn
+
+def get_dice_loss(ignore_index=255):
+    """Get Dice loss from segmentation_models_pytorch library.
+    
+    Args:
+        ignore_index (int): Class index to ignore during loss computation.
+    Returns:
+        smp.losses.DiceLoss: Dice loss function.
+    """
+    return smp.losses.DiceLoss(
+        mode='multiclass',  # Since our output has one channel per class
+        from_logits=True,
+        ignore_index=ignore_index    
+    )
+    
+    
+def get_iou_metric(ignore_index=255):
+    """Get IoU metric from torchmetrics library. 
+    This function is updated each batch with .update() and the final result is obtained with compute().
+    
+    Args:
+        ignore_index (int): Class index to ignore during metric computation.
+    
+    Returns:
+        torchmetrics.JaccardIndex: IoU metric function.
+    """
+    return JaccardIndex(task="multiclass", num_classes=2, ignore_index=ignore_index).to("cuda")
+
+def get_combined_loss(factor_BCE, factor_DICE,ignore_index=255):
+    """Get a combined loss function that sums Dice loss and Cross Entropy loss.
+    
+    Args:
+        factor_BCE (float): Weight for the Cross Entropy loss.
+        factor_DICE (float): Weight for the Dice loss.
+        ignore_index (int): Class index to ignore during loss computation.
+    Returns:
+        function: Combined loss function.
+    """
+    
+    assert factor_BCE + factor_DICE == 1.0, "The sum of factor_BCE and factor_DICE must be 1.0"
+    dice_loss_fn = get_dice_loss(ignore_index=ignore_index)
+    ce_loss_fn = nn.CrossEntropyLoss(ignore_index=ignore_index)
+    
+    def combined_loss(preds, targets):
+        loss_dice = dice_loss_fn(preds, targets)
+        loss_ce = ce_loss_fn(preds, targets)
+        return factor_DICE * loss_dice + factor_BCE * loss_ce
+    
+    return combined_loss
---
+++ models/utils/training.py
@@ -0,0 +1,80 @@
+# Utilities for the training loop
+from tqdm import tqdm
+import torch
+
+def train_one_epoch(model, loader, optimizer, criterion, device):
+    """Runs one epoch of training.
+
+    Args:
+        model (torch.nn.Module): The model to train.
+        loader (torch.utils.data.DataLoader): DataLoader for the training data.
+        optimizer (torch.optim.Optimizer): Optimizer for updating model parameters.
+        criterion (torch.nn.Module): Loss function.
+        device (torch.device): Device to run the training on ("cuda" or "cpu").
+    Returns:
+        float: Average loss over the epoch.
+    """
+    
+    model.train()
+    running_loss = 0.0
+    
+    pbar = tqdm(loader, desc="Training", leave=False, position=1)
+    for images, masks in pbar:
+        images, masks = images.to(device), masks.to(device)
+        
+        optimizer.zero_grad()
+        
+        # Forward
+        outputs = model(images) # /!\ (B, 2, H, W) -> 1 channel per class
+        
+        loss = criterion(outputs, masks)
+
+        # Backward
+        loss.backward()
+        optimizer.step()
+        
+        running_loss += loss.item()
+        pbar.set_postfix({"loss": f"{loss.item():.4f}"})
+    
+    # Average loss
+    return running_loss / len(loader)
+
+
+def validate_one_epoch(model, loader, criterion, iou_metric, device):
+    """Runs one epoch of validation. Computes both loss and IoU metric.
+
+    Args:
+        model (torch.nn.Module): The model to validate.
+        loader (torch.utils.data.DataLoader): DataLoader for the validation data.
+        criterion (torch.nn.Module): Loss function.
+        iou_metric (torchmetrics.JaccardIndex): IoU metric function.
+        device (torch.device): Device to run the validation on ("cuda" or "cpu").
+    Returns:
+        tuple: Average loss and IoU over the epoch.
+    """
+    
+    model.eval()
+    running_loss = 0.0
+    
+    # reset IoU
+    iou_metric.reset()
+    
+    pbar = tqdm(loader, desc="Validation", leave=False, position=1)
+    with torch.no_grad():
+        for images, masks in pbar:
+            images, masks = images.to(device), masks.to(device)
+            
+            # Forward
+            outputs = model(images)  # /!\ (B, 2, H, W) -> 1 channel per class
+            
+            loss = criterion(outputs, masks)
+            running_loss += loss.item()
+            pbar.set_postfix({"loss": f"{loss.item():.4f}"})
+            # Update IoU metric
+            iou_metric.update(outputs, masks)
+            
+    # Average loss
+    avg_loss = running_loss / len(loader)
+    # Compute IoU
+    avg_iou = iou_metric.compute().item()
+    return avg_loss, avg_iou
